65fcdf8713c85ea9ed8739c9cca6a8b4336cb3de
MD Rakibul Hasan Naym <73577646+RH-NAYM@users.noreply.github.com>
2023-08-21 14:25:03 +0600

    Update asynchronous_api.py

--- asynchronous_api.py
+++ asynchronous_api.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import asyncio
 from typing import List, Union
-from test_function import *
+from asynchronus_function import *
+import uvicorn
 
 app = FastAPI()
 
@@ -11,9 +12,14 @@ class Item(BaseModel):
     sequence: list
 
 async def process_item(item: Item):
-    result = await mainDetect(item.url, item.sequence)
-    result = json.loads(result)
-    return result
+    try:
+        result = await mainDetect(item.url, item.sequence)
+        # print(item.url)
+        result = json.loads(result)
+        return result
+    finally:
+        torch.cuda.empty_cache()
+        pass
 
 async def process_items(items: Union[Item, List[Item]]):
     if isinstance(items, list):
@@ -22,18 +28,28 @@ async def process_items(items: Union[Item, List[Item]]):
         results = {}
         for item in results_dict:
             results.update(item)
-
-
     else:
         results = await process_item(items)
     return results
 
-@app.post("/test")
+@app.post("/AI_Detection")
 async def create_items(items: Union[Item, List[Item]]):
-    results = await process_items(items)
-    print("Result Sent to User:", results)
-    print("###################################################################################################")
+    try:
+        results = await process_items(items)
+        print("Result Sent to User:", results)
+        print("###################################################################################################")
+        print(items)
+        return results
+    finally:
+        torch.cuda.empty_cache()
+        pass
 
-    return results
+if __name__ == "__main__":
+    try:
+        del Brand_model
+        del Count_model
+        uvicorn.run(app, host="127.0.0.1", port=8000)
+    finally:
+        torch.cuda.empty_cache()
 
 
